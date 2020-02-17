ae0ae865f817a512c38e27aa6c5a236b894c20a7
MinexAutomation <minex.automation@gmail.com>
2020-02-17 15:44:27 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Angleterria.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.Angleterria.Default;
+
+
+namespace R5T.Angleterria.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioStringlyTypedPathPartsOperator"/> service.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioStringlyTypedPathPartsOperator(this IServiceCollection services)
+        {
+            services.AddDefaultVisualStudioStringlyTypedPathPartsOperator();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioStringlyTypedPathPartsOperator"/> service.
+        /// </summary>
+        public static ServiceAction<IVisualStudioStringlyTypedPathPartsOperator> AddVisualStudioStringlyTypedPathPartsOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioStringlyTypedPathPartsOperator>(() => services.AddVisualStudioStringlyTypedPathPartsOperator());
+            return serviceAction;
+        }
+    }
+}
